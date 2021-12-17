246be4ffe2037494baf56aeb36d0be9dfa3195d1
Vallagarde <val.lagarde29@gmail.com>
2021-12-17 14:03:52 +0100

    panier ou il manque la suppression en HTML

--- Projet2/Models/BddContext.cs
+++ Projet2/Models/BddContext.cs
@@ -43,7 +43,7 @@ namespace Projet2.Models
         {
             this.Database.EnsureDeleted();
             this.Database.EnsureCreated();
-<<<<<<< HEAD
+
             this.PaniersSaisonniers.AddRange(
 
             new PanierSaisonnier { 
@@ -59,8 +59,7 @@ namespace Projet2.Models
             );
 
             this.SaveChanges();
-=======
->>>>>>> 3cbf43c6898b2b408746d508589c13f84fc7b372
+
         }
     }
 }
