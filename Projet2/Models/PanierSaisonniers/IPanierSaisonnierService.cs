fa2e3b04eef7e22e619e72f926cb11e0a5c0f803
JeyanC <jcakirlar@gmail.com>
2021-12-22 10:01:59 +0100

    fix panier

--- Projet2/Models/PanierSaisonniers/IPanierSaisonnierService.cs
+++ Projet2/Models/PanierSaisonniers/IPanierSaisonnierService.cs
@@ -9,7 +9,7 @@ namespace Projet2.Models.PanierSaisonniers
     {
         void DeleteCreateDatabase();
         List<PanierSaisonnier> ObtientTousLesPaniers();
-        int CreerPanierSaisonnier(string nomPanier, string produitsProposes, string description, decimal prix, string image);
+        int CreerPanierSaisonnier(string nomPanier, string produitsProposes, string description, decimal prix, string image, int adpId);
         int CreerPanierSaisonnier(PanierSaisonnier panierSaisonnier);
         int ModifierPanierSaisonnier(int Id, string nomPanier, string produitsProposes, string description, decimal prix);
         PanierSaisonnier ModifierPanierSaisonnier(PanierSaisonnier panierSaisonnier);
